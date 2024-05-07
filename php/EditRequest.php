bebb4c1c606be800431070f42120520272d1e34d
sarahAAD <145826237+sarahAAD@users.noreply.github.com>
2024-05-07 23:12:00 +0300

    Update EditRequest.php

--- php/EditRequest.php
+++ php/EditRequest.php
@@ -63,9 +63,9 @@ if (!isset($_SESSION['user_id'])) {
                          <nav id="account-nav"><img src="../images/<?php echo  $image?>" id="account-img">
                             <ul>
                                 
-                                <li class="account-list"><a href="#"><div class="circle"></div>Edit Account</a></li>
-                                
-                                <li class="account-list"><a href="#"><div class="circle"></div>Log Out</a></li>
+                               <li class="account-list"><a href="EditProfile.php"><div class="circle"></div>Edit Profile</a></li>
+                            
+                            <li class="account-list"><a href="logout.php"><div class="circle"></div>Log Out</a></li>
                             </ul>
 
                         </nav>
