cf510fd58b80d80e5be4f8253d138dac9b3933f1
joury <146828726+jourys@users.noreply.github.com>
2024-05-07 15:43:39 +0300

    Update HomePageLearner.php

--- php/HomePageLearner.php
+++ php/HomePageLearner.php
@@ -19,11 +19,11 @@ include("learnerInfo.php");
 <head>
   <meta charset="utf-8">
   <title>Learner Home</title>
-  <link rel="stylesheet" type="text/css" href="css/HomePageLearner.css">
-  <link rel="stylesheet" href="css/footer.css">
-  <link rel="stylesheet" type="text/css" href="css/sidebar-tutor.css">
+  <link rel="stylesheet" type="text/css" href="../css/HomePageLearner.css">
+  <link rel="stylesheet" href="../css/footer.css">
+  <link rel="stylesheet" type="text/css" href="../css/sidebar-tutor.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-  <link rel="stylesheet" href="header_folder/headerPartner.css">
+  <link rel="stylesheet" href="../header_folder/headerPartner.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
 </head>
