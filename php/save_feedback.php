d03eb4797eea199decfac3dfd5e363f7e5c02931
joury <146828726+jourys@users.noreply.github.com>
2024-05-07 17:24:29 +0300

    Update save_feedback.php

--- php/save_feedback.php
+++ php/save_feedback.php
@@ -3,30 +3,22 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "linguist";
-//include('linguist.sql');
 
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
-    
-    echo "Connected successfully";
-    
-// Get data from POST request
 $sessionID = $_POST["sessionID"];
 $text = $_POST["text"];
 $rating = $_POST["rating"];
 
-// Prepare and execute INSERT query
-$sql = "INSERT INTO review (sessionID, text, rating) VALUES ($sessionID, $text, $rating)";
+$sql = "INSERT INTO review (sessionID, text, rating) VALUES (?, ?, ?)";
 $stmt = $conn->prepare($sql);
-$stmt->bind_param("sss", $sessionID, $text, $rating);
+$stmt->bind_param("iss", $sessionID, $text, $rating);
 $stmt->execute();
 
-// Check for successful insertion
 if ($stmt->affected_rows > 0) {
   echo "Feedback saved successfully!";
 } else {
@@ -35,5 +27,4 @@ if ($stmt->affected_rows > 0) {
 
 $stmt->close();
 $conn->close();
-
 ?>
