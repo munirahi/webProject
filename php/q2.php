0e63c6864475218d5f36479f9d2e5e1ba6ed29f9
joury <146828726+jourys@users.noreply.github.com>
2024-05-08 00:13:30 +0300

    Create q2.php

---
+++ php/q2.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+     <link rel="stylesheet" href="../css/questions.css">
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../header_folder/headerPartner.css" />
+    <link rel="stylesheet" href="../css/footer.css">
+    <!-- icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       
+    <title>
+        Tips for getting started on linguist?
+    </title>
+</head>
+
+<body>
+  <div class="backg">
+      
+
+
+<header>
+            <div id="header-div">
+                <nav class="fixed-top" id="main-nav">
+                    <ul id="ul1">
+                        <li><img src="../images/linguistBlueAndWhite.jpg" alt="LINGUIST logo"  id="logo-img"></li>
+                        <li class="list1-item"><a href="#" class="list1-item">Home</a></li>
+                        <li class="list1-item"><a href="SESSionTutor.php">Sessions</a></li>
+                        <li class="list1-item"><a href="tutorReq.php">Requests</a></li>
+                        <li class="list1-item"><a href="toturRate.php">Rates and Reviews</a></li>
+                        <li class="list1-item">  
+                           <form action="tutor_profile_page.php" method="post">
+                              <input type="hidden" name="tutor_id" value="<?php echo $user_id; ?>">
+                              <button type="submit" class="list1-item">rrtrtsrhsmlth</button>
+                          </form></li>
+
+                        <li class="list1-item"><a href="SupportsPartner.php">Support</a></li>
+                    </ul>
+                    <ul id="ul2">
+                        
+                        <li id="acnt li">
+                            <nav id="account-nav"><img src="../images/<?php echo  $image?>" id="account-img">
+                                <ul>
+                                    
+                                    <li class="account-list"><a href="EditProfileP.php"><div class="circle"></div>Edit Profile</a></li>
+                                    
+                                    <li class="account-list"><a href="logout.php"><div class="circle"></div>Log Out</a></li>
+                                </ul>
+            
+                            </nav>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            
+        </header>
+    <div class="que">
+    <h1>
+        Tips for getting started on linguist?
+    </h1>
+    <h2>
+        Tip #1: Try different teachers until you find a few whose teaching style suits you.<br>
+Tip #2: Take advantage of everything your package has to offer!
+    </h2><br><br><br><br><br><br><br><br><br><br><br>
+</div>
+
+    <footer>
+      <div class="main-content">
+        <div class="left box">
+          <h2>About us</h2>
+          <div class="content">
+            <p> Embark on a linguistic journey with LINGUIST. Connect with native speakers worldwide,
+                 hone your language skills through immersive experiences, and unlock new cultures from the comfort of your home.<a class="find-more" href="About_us_pageMain.html">Find more</a></p>
+            <div class="social">
+              <a href="#"><span class="fab fa-facebook-f"></span></a>
+              <a href="#"><span class="fab fa-twitter"></span></a>
+              <a href="#"><span class="fab fa-instagram"></span></a>
+              <a href="#"><span class="fab fa-youtube"></span></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="center box">
+          <h2>Address</h2>
+          <div class="content">
+            <div class="place">
+              <span class="fas fa-map-marker-alt"></span>
+              <span class="text">Riyadh, KSA</span>
+            </div>
+            <div class="phone">
+              <span class="fas fa-phone-alt"></span>
+              <span class="text">+966-505432100</span>
+            </div>
+            <div class="email">
+              <span class="fas fa-envelope"></span>
+              <span class="text"><a href="mailto:content@linguist.com"></a>
+                content@linguist.com</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="right box">
+          <h2>Contact us</h2>
+          <div class="content">
+            <form action="#" method="post">
+              <div class="email">
+                <div class="text">Email <span class="required">*</span></div>
+                <input type="email" required>
+              </div>
+              <div class="msg">
+                <div class="text">Message <span class="required">*</span></div>
+                <textarea rows="2" cols="25" required></textarea>
+              </div>
+              <div class="btn">
+                <button type="submit">Send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div class="bottom">
+        <section>
+          <span class="credit">Created By <a href="main.html">linguist</a> | </span>
+          <span class="far fa-copyright"></span><span> 2024 All rights reserved.</span>
+        </section>
+      </div>
+    </footer></div>
+  </body>
+</html>
